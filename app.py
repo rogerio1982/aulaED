4e723248b4ad6e5800028e3129abac0575829df6
Rogério Soares <rogerio_si@yahoo.com.br>
2021-01-21 14:35:08 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana'] #criar lista
+print(fruits) #print
+
+
+print(fruits.count('apple')) #contado
+print(fruits.index('orange'))#index
+fruits.reverse() #ordem
+print(fruits)
+fruits.append('grape')#inserir
+print(fruits)
+fruits.sort() #ordenando
+print(fruits)
+print(fruits.pop()) #exlcuindo
+print(fruits)
