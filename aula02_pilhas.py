7c9d64fee5cd6630b225f9e3bf554deaf3933896
Rogério Soares <rogerio_si@yahoo.com.br>
2021-01-28 15:55:25 -0300

    Add files via upload

---
+++ aula02_pilhas.py
@@ -0,0 +1,16 @@
+stack = [frente, direita, esquerda, frente,frente]
+print(stack)
+
+stack.append(6)
+stack.append(7)
+print(stack)
+
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+print(stack)
+
+stack.append(4)
+stack.append(5)
+print(stack)
